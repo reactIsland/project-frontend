bfef2a258dad7707f18d232e144848278c039368
sarambru <sarambiru@gmail.com>
2022-01-19 22:55:44 -0600

    commit

--- src/api/products.js
+++ src/api/products.js
@@ -24,3 +24,36 @@ export const showProduct = (id, user) => {
       }
     })
 }
+
+// INDEX Products
+export const indexProducts = (user) => {
+  return axios.get(apiUrl + '/products/',
+    {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    }
+  )
+}
+
+// UPDATE Product
+export const updateProduct = (user, id, name, description, price, category) => {
+  return axios.patch(
+    `${apiUrl}/products/${id}`,
+    { product: { name, description, price, category } },
+    {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    }
+  )
+}
+
+// DELETE Product
+export const deleteProduct = (id, user) => {
+  return axios.delete(`${apiUrl}/products/${id}`, {
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
