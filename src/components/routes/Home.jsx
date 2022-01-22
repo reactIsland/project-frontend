c7ac1d925b1bed7f24f32eb8ab76bb6b88f58ba6
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-01-22 13:10:56 -1000

    sara matt ian kian frontend lessgooo

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Product from './Product'
+
+//use useeffect to call api to get all products
+//create state products, setProducts 
+//once you have all products put into products state
+//map all products with Product component, pass in necessary props
 
 const Home = () => (
   <>
---
+++ src/components/routes/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+    <>
+      <p>produt name</p>
+      <p>product description</p>
+      <p>product category</p>
+      <p>product price</p>
+      <button>Add to cart</button>
+    </>
+}
+
+export default Product
